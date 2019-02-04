a997df94404f9c51e0a63d8220d1fee74d92cc85
OsDev <satran004@gmail.com>
2019-02-04 18:59:06 +0800

    Project setup

---
+++ src/main/java/org/aion4j/avm/idea/HelloAction.java
@@ -0,0 +1,20 @@
+package org.aion4j.avm.idea;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import org.jetbrains.annotations.NotNull;
+
+public class HelloAction extends AnAction {
+    public HelloAction() {
+        super("Hello");
+    }
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        Project project = e.getProject();
+        Messages.showMessageDialog(project, "Hello world!", "Greeting", Messages.getInformationIcon());
+    }
+
+}
