58fc8be1c97f78cfffee03cfd6a076d637239985
OsDev <satran004@gmail.com>
2019-05-13 19:57:22 +0800

    Archetype version upgraded to 0.19

--- src/main/java/org/aion4j/avm/idea/maven/AVMArchTypeProvider.java
+++ src/main/java/org/aion4j/avm/idea/maven/AVMArchTypeProvider.java
@@ -19,7 +19,7 @@ import java.util.*;
 public class AVMArchTypeProvider implements MavenArchetypesProvider {
     private final static Logger log = Logger.getInstance(AVMArchTypeProvider.class);
 
-    private final String AVM_ARCHTYPE_VERSION = "0.17";
+    private final String AVM_ARCHTYPE_VERSION = "0.19";
 
     @Override
     public Collection<MavenArchetype> getArchetypes() {
