6cd00700b0a9f5cf09a353e624c994c3ee9d8e39
Satya <satran004@gmail.com>
2019-10-05 15:49:11 +0800

    Add account chooser for remote getbalance action

--- src/main/java/org/aion4j/avm/idea/action/remote/ui/GetAccountDialog.java
+++ src/main/java/org/aion4j/avm/idea/action/remote/ui/GetAccountDialog.java
@@ -2,18 +2,33 @@ package org.aion4j.avm.idea.action.remote.ui;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
+import org.aion4j.avm.idea.action.account.AccountChooser;
+import org.aion4j.avm.idea.action.account.model.Account;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GetAccountDialog extends DialogWrapper {
     private JTextField accountTf;
     private JPanel mainPanel;
+    private JButton selectAccountButton;
 
     public GetAccountDialog(Project project) {
         super(project, false);
         init();
         setTitle("Enter Account");
+
+        selectAccountButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Account selectedAccount = AccountChooser.getSelectedAccount(project, false);
+
+                if(selectedAccount != null)
+                    accountTf.setText(selectedAccount.getAddress());
+            }
+        });
     }
 
     @Nullable
